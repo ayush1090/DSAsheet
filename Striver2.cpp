1e0f50e8d4845fab637cd7cfe2e0f2d6117d8006
ayush1090 <105165115+ayush1090@users.noreply.github.com>
2022-05-08 21:24:59 +0530

    Add files via upload

---
+++ Striver2.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+
+int main() {
+
+	#ifndef ONLINE_JUDGE
+
+	freopen("input1.txt", "r" , stdin);
+	freopen("routput1.txt", "w" , stdout);
+
+#endif
+
+    int t; cin>>t;
+
+    while(t--)
+    {
+    	int n; cin>>n;
+
+    	ll ans = n * 50 * 0.30;
+
+    	cout<<ans<<"\n";
+
+    }
+    
+}
